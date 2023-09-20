69c1080cbe8ecf68773446e2fe535037ae5242e5
Ikap <ikapurwanti144@gmail.com>
2023-09-20 21:21:46 +0700

    update home fragment dan product adapter

--- app/src/main/java/com/binar/hackaton3_groupb/presentation/fragmenthome/FragmentHome.kt
+++ app/src/main/java/com/binar/hackaton3_groupb/presentation/fragmenthome/FragmentHome.kt
@@ -5,17 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+
 import com.binar.hackaton3_groupb.R
+import com.binar.hackaton3_groupb.data.ProductDataSourceImpl
 import com.binar.hackaton3_groupb.presentation.fragmenthome.adapter.ProductListAdapter
+import com.binar.hackaton3_groupb.databinding.FragmentHomeBinding
+import com.binar.hackaton3_groupb.model.Product
 
 class FragmentHome : Fragment() {
 
 
+    private lateinit var binding: FragmentHomeBinding
+
     private val adapterProduct: ProductListAdapter by lazy {
         ProductListAdapter {
-           // navigateToFragmentDetailMenu(it)
+            navigateToFragmentDetailMenu(it)
         }
     }
+
+    private fun navigateToFragmentDetailMenu(product: Product) {
+        val action = FragmentHomeDirections.navigateToFragmentDetail(product)
+        findNavController().navigate(action)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -31,11 +45,11 @@ class FragmentHome : Fragment() {
     }
 
     private fun showListProduct() {
-//        binding.rvProduct.adapter = adapterProduct
-//        binding.rvProduct.layoutManager =
-//            GridLayoutManager(requireContext(), 2 )
-//        adapterProduct.setData(ProductDataSourceImpl().getProduct())
-//
+        binding.rvProduct.adapter = adapterProduct
+        binding.rvProduct.layoutManager =
+            GridLayoutManager(requireContext(), 2 )
+        adapterProduct.setData(ProductDataSourceImpl().getProductData())
+
     }
 
 
