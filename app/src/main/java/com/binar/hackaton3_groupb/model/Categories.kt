98fdc78fe51e31ac76e00e78be8cf7262d2ebb47
doantaa <doantaaloycius@gmail.com>
2023-09-20 21:09:21 +0700

    Create Main nav Graph

--- app/src/main/java/com/binar/hackaton3_groupb/model/Categories.kt
+++ app/src/main/java/com/binar/hackaton3_groupb/model/Categories.kt
@@ -1,9 +1,11 @@
 package com.binar.hackaton3_groupb.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import java.util.UUID
-
+@Parcelize
 data class Categories(
     val id: String = UUID.randomUUID().toString(),
     val name: String,
     val imageUrl: String
-)
+):Parcelable
--- app/src/main/java/com/binar/hackaton3_groupb/model/Product.kt
+++ app/src/main/java/com/binar/hackaton3_groupb/model/Product.kt
@@ -1,7 +1,9 @@
 package com.binar.hackaton3_groupb.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import java.util.UUID
-
+@Parcelize
 data class Product(
     val id: String = UUID.randomUUID().toString(),
     val name: String,
@@ -11,4 +13,4 @@ data class Product(
     val rating: Double,
     val description: String,
     val imageUrl: String
-)
+): Parcelable
